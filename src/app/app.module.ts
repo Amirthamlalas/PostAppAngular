952f09d65c4eb5ccfd121d2f54fe5c2044bf4562
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-11-25 15:46:48 +0530

    added navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddPostComponent } from './add-post/add-post.component';
 import { ViewPostComponent } from './view-post/view-post.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const myroute : Routes=[
+  {
+    path : "",
+    component : AddPostComponent
+  },
+  {
+    path : "view",
+    component : ViewPostComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
     AddPostComponent,
-    ViewPostComponent
+    ViewPostComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
